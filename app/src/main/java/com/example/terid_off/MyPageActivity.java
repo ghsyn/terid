4597a8365fd35005c667aff8dd328ef410eed487
codefordmr01 <codefordmr01@naver.com>
2023-01-08 19:45:25 +0900

    third_addcalendar

--- app/src/main/java/com/example/terid_off/MyPageActivity.java
+++ app/src/main/java/com/example/terid_off/MyPageActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyPageActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    CalendarView cal;
+    TextView nowdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,11 @@ public class MyPageActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+
+        cal = findViewById(R.id.cal);
+        nowdate = findViewById(R.id.nowdate);
+
+        nowdate.setText(getTime());
 
     }
 
@@ -30,5 +42,14 @@ public class MyPageActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String getTime = dateFormat.format(date);
+
+        return getTime;
     }
 }
